863556cff0a14c69cbc0535c5070db60e3db98c6
Alexandre Boccard <aboccard@bu.edu>
2020-12-12 13:37:30 -0500

    Create GOOGLE.py

--- main/search/YELP.py
+++ main/search/YELP.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 import os
-import json
-import requests
+
 from yelpapi import YelpAPI
 
 
@@ -9,11 +8,11 @@ load_dotenv()
 
 YELP_KEY = str(os.getenv('YELP_KEY'))
 
-yelp_api = YelpAPI(YELP_KEY)
+yelp = YelpAPI(YELP_KEY)
 
 def YELP_SEARCH(latitude, longitude, radius = 4828, price = None):
 
-	return yelp_api.search_query(latitude = latitude, longitude = longitude, radius = radius, price = price)
+	return yelp.search_query(latitude = latitude, longitude = longitude, radius = radius, price = price)
 
 
 
