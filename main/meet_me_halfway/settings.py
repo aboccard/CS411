7c81401eeb41f5e5465ad702d65fbd82dacb7121
Alexandre Boccard <aboccard@bu.edu>
2020-12-14 20:10:21 -0500

    create login and user modules

--- main/meet_me_halfway/settings.py
+++ main/meet_me_halfway/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import allauth
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -125,7 +126,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
