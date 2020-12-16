2c664950b94529afcc87f5aa605992465e1fcede
Alexandre Boccard <aboccard@bu.edu>
2020-12-15 20:16:28 -0500

    fix connection

--- main/meet_me_halfway/urls.py
+++ main/meet_me_halfway/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
     path('accounts/', include('allauth.urls')),
+    path('search/', include('search.urls')),
 ]
